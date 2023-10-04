c2de1e0735445f0cc4e10466cb7e1c6bfc9a2a92
yeolife <yeo2507@naver.com>
2023-10-04 23:56:34 +0900

    1. 서버에서 데이터 받아올 때,  async를 사용해서 예상치 못한 오류로부터 더욱 안전하게 보호

---
+++ Appetizers/Function/CustomModifiers.swift
@@ -0,0 +1,18 @@
+//
+//  CustomModifiers.swift
+//  Appetizers
+//
+//  Created by yeolife on 10/4/23.
+//
+
+import SwiftUI
+
+struct CustomModifiers: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomModifiers()
+}
